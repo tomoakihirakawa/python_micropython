74af2c2a8ab744c33c135b26bbc6175d01c38368
Tomoaki Hiralawa <takayuki81610@gmail.com>
2021-12-01 13:36:36 +0900

    圧力センサーのリアルタイム計測用スクリプトを追加

--- example_PressureSensor/Pressure_check_realtimeplot_stand_alone.py
+++ example_PressureSensor/Pressure_check_realtimeplot_stand_alone.py
@@ -14,38 +14,18 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set(xlabel='time [s]', ylabel='pressure [Pa]')
 # -------------------------------------------------------- #
-period = 0.07
+period = 0.01
 
-P0 = []
-P1 = []
-P2 = []
-P3 = []
-P4 = []
-P5 = []
-P6 = []
+N_sensors = 7
 
-T0 = []
-T1 = []
-T2 = []
-T3 = []
-T4 = []
-T5 = []
-T6 = []
-
+Ps = [[] for i in range(N_sensors)]
+Ts = [[] for i in range(N_sensors)]
 
 colors = ['green', 'blue', 'red', 'yellow',
           'black', 'orange', 'magenta', 'cyan']
-line0, = ax.plot(T0, P0, color=colors[0], label=colors[0])
-line1, = ax.plot(T1, P1, color=colors[1], label=colors[1])
-line2, = ax.plot(T2, P2, color=colors[2], label=colors[2])
-line3, = ax.plot(T3, P3, color=colors[3], label=colors[3])
-line4, = ax.plot(T4, P4, color=colors[4], label=colors[4])
-line5, = ax.plot(T5, P5, color=colors[5], label=colors[5])
-line6, = ax.plot(T6, P6, color=colors[6], label=colors[6])
 
-lines = [line0, line1, line2, line3, line4, line5, line6]
-Ps = [P0, P1, P2, P3, P4, P5, P6]
-Ts = [T0, T1, T2, T3, T4, T5, T6]
+lines = [ax.plot(Ts[i], Ps[i], color=colors[i], label=colors[i])[0]
+         for i in range(N_sensors)]
 
 start = time_ns()
 count = 0
@@ -55,7 +35,6 @@ while count < 5000:
     sleep(period)
     print(count)
     try:
-        # data = sensors[i]()
         current_time = (time_ns()-start)*10**-9
         data = {"depth": sin(current_time)}
         print(data)
@@ -68,7 +47,7 @@ while count < 5000:
         ax.relim()
         ax.autoscale()
         fig.canvas.draw()
-        plt.pause(0.0001)
+        plt.pause(0.001)
         plt.legend()
     except KeyboardInterrupt:
         plt.close('all')
