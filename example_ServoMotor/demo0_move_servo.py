44cde556995a58e08085a2987fb6971ad027347f
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-15 17:44:11 +0900

    update servomotor

--- example_ServoMotor/demo0_move_servo.py
+++ example_ServoMotor/demo0_move_servo.py
@@ -17,12 +17,46 @@ from lib.servomotor import *
 ```
 
 上の命令で`lib`内の`servomotor`ディレクトリにある`__init__.py`が実行される．
-`__init__.py`には，`from .servomotor import *`という命令が書かれており，
-`lib`内の`servomotor`内の`servomotor.py`に書かれている関数やクラスが，そのまま使えるようになる．
+
+`__init__.py`には，`from .servomotor import *`という命令が書かれている．
+この意味は，`lib`内の`servomotor`内の`servomotor.py`に書かれている関数やクラスを全てインポートするという意味である．
+これで，`servomotor.py`内の`servomotor`クラスを使うことができる．
 
 \insert{servomotor_calss}
 
 ## サーボモーター1つの制御
+
+<details>
+
+---
+
+<summary>Python パッケージ</summary>
+
+あるディレクトリに，`__init__.py`というファイルがあると，そのディレクトリは**Pythonのパッケージ**となる．
+
+```
+from パッケージ名 import *
+```
+
+とすることで，そのパッケージ内の`__init__.py`がまず実行され，それに従って，パッケージ内のモジュールがインポートされる．
+ここでは，`lib.servomotor`をパッケージとしてインポートしている．
+
+---
+
+</details>
+
+ここでは，以下のようにインポートしたが
+
+```
+from lib.servomotor import *
+```
+
+`lib/servomotor/servomotor.py`から`servomotor`クラスをインポートする，という意味で，次のようにもできる．
+これで同じように`s = servomotor(0, 90)`としてサーボモーターを作成できる．
+
+```
+from lib.servomotor.servomotor import servomotor
+```
 
 ![](sample.gif)
 
